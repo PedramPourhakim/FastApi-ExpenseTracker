93ba4b7f06721b553c0e831144cac0fd28329ca1
Pedram_Pourhakim <81464138+PedramPourhakim@users.noreply.github.com>
2026-05-02 17:31:55 +0330

    updated config default urls for test failure

--- app/core/config.py
+++ app/core/config.py
@@ -3,11 +3,11 @@ import secrets
 
 
 class Settings(BaseSettings):
-    SQLALCHEMY_DATABASE_URL: str
+    SQLALCHEMY_DATABASE_URL: str = "sqlite:///:memory:"
     JWT_SECRET_KEY: str = secrets.token_urlsafe(64)
     JWT_ALGORITHM: str = "HS256"
     model_config = SettingsConfigDict(env_file=".env")
-    REDIS_URL: str
+    REDIS_URL: str = "redis://redis:6379"
     SENTRY_DSN: str
 
 settings = Settings()
